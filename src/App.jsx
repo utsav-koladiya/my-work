11d42de92d9d44d0e85b1a2f209317abb169256e
utsav-koladiya <utsavkoladiya9@gmail,com>
2025-11-10 19:22:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my React application.</p>
+    </div>
+        
+  )
+}
+
+export default App
